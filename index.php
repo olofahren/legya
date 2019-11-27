2dc94e657635cf46125a2a51222de0b9d68c8e0e
Olof Åhrén <olof.murgarden@gmail.com>
2019-11-27 16:36:05 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Legya</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="menu">
+            <img src="" alt="logo" class="logo">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+            </ul>
+        </div>
+        <div class="content">
+            <div class="search">
+                <form action="addItem.php" method="POST">
+                    <input name="itemId" type="text"/>
+                    <input type="submit" value="Add item">
+                </form>
+            </div>
+            <div class="brick-list">
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
