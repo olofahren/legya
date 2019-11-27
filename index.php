e0016c8f8e7439616c8f07442e9911397039b754
Olof Åhrén <olof.murgarden@gmail.com>
2019-11-27 18:13:48 +0100

    add simple css, form(js)# with '#' will be ignored, and an empty message aborts the commit.

--- index.php
+++ index.php
@@ -1,31 +1,43 @@
+<?php
+session_start();
+$_SESSION["items"];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <title>Legya</title>
 </head>
 <body>
     <div class="wrapper">
-        <div class="menu">
+        <div class="header">
             <img src="" alt="logo" class="logo">
-            <ul>
+            <ul class="menu">
                 <li>Home</li>
                 <li>About</li>
             </ul>
         </div>
         <div class="content">
             <div class="search">
-                <form action="addItem.php" method="POST">
-                    <input name="itemId" type="text"/>
-                    <input type="submit" value="Add item">
+                <h2>Add lego brick</h2>
+                <div class="form">
+                    <input onkeypress="addItem()"id="itemIdInput"name="itemId" type="text"/>
+                    <button onclick="addItemClick()">Add item</button>
+                </div>
                 </form>
             </div>
             <div class="brick-list">
-
             </div>
         </div>
     </div>
+
+    <script src="script.js"></script>
+
 </body>
 </html>
---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+console.log("katt");
+var tempItems = [];   //declare an array
+
+function addItem(){
+    //add check to not add already existing item
+
+document.getElementById('itemIdInput').onkeydown = function(e){
+        if(e.keyCode == 13){
+            var itemId = document.getElementById("itemIdInput").value;  //get the value of an element by it's id
+            tempItems.push(itemId); //put to the array
+            console.log(tempItems);
+            document.getElementById('itemIdInput').value = '';
+
+        }
+    }
+
+}
+
+function addItemClick(){
+    var itemId = document.getElementById("itemIdInput").value;  //get the value of an element by it's id
+    tempItems.push(itemId); //put to the array
+    console.log(tempItems);
+    document.getElementById('itemIdInput').value = '';
+}
+
+
+ 
