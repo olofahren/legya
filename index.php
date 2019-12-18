3a363349a3ede8fa76042a0e91d46115f52a1f6b
Olof Åhrén <oloah408@student.liu.se>
2019-12-18 11:41:57 +0100

    added more css animations

--- index.php
+++ index.php
@@ -21,57 +21,19 @@ if ($_GET["key"]) {
 <body>
     <div class="wrapper">
        <?php include("menu.html");
+        if ($_GET["key"]) {
+            include("sort.php");
+        }
+       
        ?>
-       <div class="sorting">
-
-       <!-- LIMIT -->
-                <div class="select-display-amount" >
-                    <form method="GET"> 
-                        <input type="number" name="limit">
-                        <input type="hidden" id="keyHidden" name="key" value="<?php echo $_GET["key"]?>">
-                        <input type="hidden" id="orderHidden" name="order" value="<?php echo $_GET["order"]?>">
-                        <input type="hidden" id="sortByHidden" name="sort-by" value="<?php echo $_GET["sort-by"]?>">
-                        <input type="submit" value="Limit results">
-                    </form>
-                </div>
-        
-
-        <!-- SORT BY -->
-                <div class="sort">
-                    <form method="GET">
-                        <select name="sort">
-                            <option value="Setname">Alpabetical</option>
-                            <option value="Year">Release date</option>
-                        </select>
-                        <input type="hidden" id="keyHidden" name="key" value="<?php echo $_GET["key"]?>">
-                        <input type="hidden" id="limitHidden" name="limit" value="<?php echo $_GET["limit"]?>">
-                        <input type="hidden" id="orderHidden" name="order" value="<?php echo $_GET["order"]?>">
-
-                        <input type="submit" value="Change sorting">
-                    </form>
-                </div>
-        <!-- ASC/DESC -->
-                <div class="order">
-                    <form method="GET">
-                        <select name="order">
-                            <option value="desc">Descending</option>
-                            <option value="asc">Ascending</option>
-                        </select>
-                        <input type="hidden" id="keyHidden" name="key" value="<?php echo $_GET["key"]?>">
-                        <input type="hidden" id="limitHidden" name="limit" value="<?php echo $_GET["limit"]?>">
-                        <input type="hidden" id="sortHidden" name="sort" value="<?php echo $_GET["sort"]?>">
-
-                        <input type="submit" value="Change sorting">
-                    </form>
-                </div>
-
-        </div>
-
+       
         <div class="content">
             
             <div class="search">
-            <img class="airplane" src="https://img.pngio.com/archives-aircraft-cirrus-aircraft-propeller-plane-side-png-750_398.png" alt="">
-                <h2>Search </h2>
+            <img class="airplane" src="https://ideascdn.lego.com/media/generate/lego_ci/7b115add-2662-4114-b611-2786e9045efd/resize:800:450" alt="">
+            <img src="http://www2.leaseweb.com/rs/326-LAN-349/images/Hybrid_lego_2.png" alt="" class="cloud">
+            <img src="http://www2.leaseweb.com/rs/326-LAN-349/images/Hybrid_lego_2.png" alt="" class="cloud2">
+                <h2>Search</h2>
                 <div class="form">
                     <form method="GET" action="">
                             <input type="text" id="searchField" name="key">
