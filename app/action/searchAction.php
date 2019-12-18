d2471af905efba6f4dbd6a1714954ddb7c64f26c
Olof Åhrén <oloah408@student.liu.se>
2019-12-18 09:20:12 +0100

    added sorting

--- app/action/searchAction.php
+++ app/action/searchAction.php
@@ -7,11 +7,27 @@ if ($_GET["limit"]) {
     $limit = filter_input(INPUT_GET, "limit", FILTER_SANITIZE_SPECIAL_CHARS);
 }
 else{
-    $limit = 20;    
+    $limit = 18;    
 }
+
+if ($_GET["sort"]) {
+    $sort = filter_input(INPUT_GET, "sort", FILTER_SANITIZE_SPECIAL_CHARS);
+}
+else{
+    $sort = "Setname";
+}
+
+if ($_GET["order"]) {
+    $order = filter_input(INPUT_GET, "order", FILTER_SANITIZE_SPECIAL_CHARS);
+}
+else{
+    $order = "desc";
+}
+
+
 $searchKey = filter_input(INPUT_GET, "key", FILTER_SANITIZE_SPECIAL_CHARS);
 
-$query = "SELECT * FROM sets WHERE Setname LIKE '%$searchKey%' LIMIT $limit";
+$query = "SELECT * FROM sets WHERE Setname LIKE '%$searchKey%' ORDER BY $sort $order LIMIT $limit ";
 $data = array($searchKey);
 
 $results= runQuery($query, true, $data);
--- index.php
+++ index.php
@@ -22,6 +22,48 @@ if ($_GET["key"]) {
     <div class="wrapper">
        <?php include("menu.html");
        ?>
+       <div class="sorting">
+
+       <!-- LIMIT -->
+                <div class="select-display-amount" >
+                    <form method="GET"> 
+                        <input type="number" name="limit">
+                        <input type="hidden" id="keyHidden" name="key" value="<?php echo $_GET["key"]?>">
+                        <input type="hidden" id="orderHidden" name="order" value="<?php echo $_GET["order"]?>">
+                        <input type="hidden" id="sortByHidden" name="sort-by" value="<?php echo $_GET["sort-by"]?>">
+                        <input type="submit" value="Limit results">
+                    </form>
+                </div>
+
+        <!-- SORT BY -->
+                <div class="sort">
+                    <form method="GET">
+                        <select name="sort">
+                            <option value="Setname">Alpabetical</option>
+                            <option value="Year">Release date</option>
+                        </select>
+                        <input type="hidden" id="keyHidden" name="key" value="<?php echo $_GET["key"]?>">
+                        <input type="hidden" id="limitHidden" name="limit" value="<?php echo $_GET["limit"]?>">
+                        <input type="hidden" id="orderHidden" name="order" value="<?php echo $_GET["order"]?>">
+
+                        <input type="submit" value="Change sorting">
+                    </form>
+                </div>
+        <!-- ASC/DESC -->
+                <div class="order">
+                    <form method="GET">
+                        <select name="order">
+                            <option value="desc">Descending</option>
+                            <option value="asc">Ascending</option>
+                        </select>
+                        <input type="hidden" id="keyHidden" name="key" value="<?php echo $_GET["key"]?>">
+                        <input type="hidden" id="limitHidden" name="limit" value="<?php echo $_GET["limit"]?>">
+                        <input type="hidden" id="sortHidden" name="sort" value="<?php echo $_GET["sort"]?>">
+
+                        <input type="submit" value="Change sorting">
+                    </form>
+                </div>
+
         <div class="content">
             <div class="search">
                 <h2>Search </h2>
