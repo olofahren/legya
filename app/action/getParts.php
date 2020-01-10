b5efd8eeb006945740c7f16cfdb8f9aabf63b3a7
John Tember <johte805@student.liu.se>
2020-01-10 12:04:59 +0100

    added gubbs

--- app/action/getParts.php
+++ app/action/getParts.php
@@ -7,10 +7,43 @@ $SetID = filter_input(INPUT_GET, "SetID", FILTER_SANITIZE_SPECIAL_CHARS);
 
 //$query = "SELECT inventory.SetID, inventory.ItemID, minifigs.Minifigname FROM inventory, minifigs
 //WHERE inventory.SetID='$SetID' AND inventory.ItemtypeID='P' AND minifigs.MinifigID=inventory.ItemID";
-$query = "SELECT inventory.SetID, inventory.Quantity, colors.Colorname, parts.Partname, inventory.ItemTypeID, inventory.ItemID, colors.ColorID FROM inventory, colors, parts 
-WHERE inventory.SetID='$SetID' AND inventory.ItemtypeID='P' AND colors.ColorID=inventory.ColorID AND parts.PartID=inventory.ItemID";
+$query = 
+"SELECT
+    inventory.SetID,
+    inventory.Quantity,
+    minifigs.Minifigname,
+    inventory.ItemTypeID,
+    inventory.ItemID
+FROM
+    inventory,
+    minifigs 
+WHERE
+    inventory.SetID='$SetID'
+AND inventory.ItemtypeID='M'
+AND minifigs.MinifigID=inventory.ItemID";
+
+$secondquery = 
+"SELECT
+    inventory.SetID,
+    inventory.Quantity,
+    colors.Colorname,
+    parts.Partname,
+    inventory.ItemTypeID,
+    inventory.ItemID,
+    colors.ColorID
+FROM
+    inventory,
+    colors,
+    parts 
+WHERE
+    inventory.SetID='$SetID'
+AND inventory.ItemtypeID='P'
+AND colors.ColorID=inventory.ColorID
+AND parts.PartID=inventory.ItemID";
+
 $data = array($SetID);
 
-$_SESSION["setParts"] = runQuery($query, true, $data);
+$_SESSION["setParts"] = runQuery($secondquery, true, $data);
+$_SESSION["setGuys"] = runQuery($query, true, $data);
 
 header("Location: ../../next2.php?SetID=$SetID");
--- next2.php
+++ next2.php
@@ -23,24 +23,36 @@ session_start();
         <div class="results">
             <h1>Parts in <?php echo $_GET["SetID"]?></h1>
             <div class="box1">
-            <table class="table">
-        <tr>
-            <th>Quantity</th>
-            <th>Picture</th>
-            <th>Color</th>
-            <th>Part name</th>
-         </tr>
-                <?php foreach ($_SESSION["setParts"] as $part) {
-        ?>
-            <tr>    
-                <td><?= $part['Quantity']?></td>
-                <td ><img class="legoImg" src="http://weber.itn.liu.se/~stegu76/img.bricklink.com/<?= $part['ItemTypeID']?>/<?= $part['ColorID']?>/<?= $part['ItemID']?>" alt="part image"></td>
-                <td><?= $part['Colorname']?></td>
-                <td><?= $part['Partname']?></td>
-                <td><?= $part['MinifigID']?></td>
-                <td><?= $part['ColorID']?></td>
-                <td><?= $part['ItemID']?></td>
-            </tr>
+                <table class="table">
+                    <tr>
+                        <th>Quantity</th>
+                        <th>Picture</th>
+                        <th>Color</th>
+                        <th>Part name</th>
+                    </tr>
+                    <?php foreach ($_SESSION["setParts"] as $part) {
+                ?>
+                <tr>    
+                    <td><?= $part['Quantity']?></td>
+                    <td ><img class="legoImg" src="http://weber.itn.liu.se/~stegu76/img.bricklink.com/<?= $part['ItemTypeID']?>/<?= $part['ColorID']?>/<?= $part['ItemID']?>" alt="part image"></td>
+                    <td><?= $part['Colorname']?></td>
+                    <td><?= $part['Partname']?></td>
+                    <td><?= $part['MinifigID']?></td>
+                    <td><?= $part['ColorID']?></td>
+                    <td><?= $part['ItemID']?></td>
+                </tr>
+            <?php }?>
+            <?php foreach ($_SESSION["setGuys"] as $part) {
+                ?>
+                <tr>    
+                    <td><?= $part['Quantity']?></td>
+                    <td ><img class="legoImg" src="http://weber.itn.liu.se/~stegu76/img.bricklink.com/<?= $part['ItemTypeID']?>/<?= $part['ItemID']?>" alt="part image"></td>
+                    <td><?= $part['Colorname']?></td>
+                    <td><?= $part['Partname']?></td>
+                    <td><?= $part['MinifigID']?></td>
+                    <td><?= $part['ColorID']?></td>
+                    <td><?= $part['ItemID']?></td>
+                </tr>
             <?php }?>
         </table>
 
